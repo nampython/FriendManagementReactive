9630a022fbe0c7d1761076455e1c327e17198106
nam.dinh <dragonsang10a1@gmail.com>
2023-05-29 11:08:05 +0700

    #14 API-7643 Implement of unit test for Block Reactive Repository Test

---
+++ src/test/java/org/example/repository/BlockReactiveRepositoryTest.java
@@ -0,0 +1,46 @@
+package org.example.repository;
+
+import junit.framework.TestCase;
+import org.example.model.friends.Block;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+@DataR2dbcTest
+@RunWith(SpringRunner.class)
+public class BlockReactiveRepositoryTest extends TestCase {
+
+    @Mock
+    private BlockReactiveRepository blockReactiveRepository;
+
+    @Test
+    public void testFindByBlockerIdAndBlockedId() {
+        // Input
+        int blockerId = 1;
+        int blockedId = 2;
+
+        // Sample Data
+        Block block = new Block();
+        block.setBlockerId(blockerId);
+        block.setBlockedId(blockedId);
+
+        // Mock
+        when(blockReactiveRepository.findByBlockerIdAndBlockedId(eq(blockerId), eq(blockedId)))
+                .thenReturn(Mono.just(block));
+
+        // Invoke method
+        Mono<Block> result = blockReactiveRepository.findByBlockerIdAndBlockedId(blockerId, blockedId);
+
+        // Verify the result
+        StepVerifier.create(result)
+                .expectNext(block)
+                .verifyComplete();
+    }
+}
