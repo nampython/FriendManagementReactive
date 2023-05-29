e983017c8764d800fa8448c447be250b09b1d36b
NamQuang13 <dragonsangsang10a1@gmail.com>
2023-05-29 21:40:17 +0700

    #24 API-5852 Fixed Missing method in Subscription Reactive Dao

--- src/main/java/org/example/repository/SubscriptionReactiveDao.java
+++ src/main/java/org/example/repository/SubscriptionReactiveDao.java
@@ -3,9 +3,11 @@ package org.example.repository;
 
 import org.example.model.friends.Subscription;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface SubscriptionReactiveDao extends R2dbcRepository<Subscription, Integer> {
     Mono<Subscription> findBySubscriberIdAndTargetId(int subscriberId, int targetId);
     Mono<Subscription> deleteBySubscriberIdAndTargetId(int subscriberId, int targetId);
+     Mono<Subscription> findBySubscriberId(int userId);
 }
