b724927cd7e7fafc411472d585d0f83c2fdc5b0a
nam.dinh <dragonsang10a1@gmail.com>
2023-05-29 10:34:00 +0700

    #10 - Implement of unit test for User Reactive Dao

---
+++ src/test/java/org/example/repository/UserReactiveDaoTest.java
@@ -0,0 +1,70 @@
+package org.example.repository;
+
+import junit.framework.TestCase;
+import org.example.model.friends.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+
+@DataR2dbcTest
+@RunWith(SpringRunner.class)
+public class UserReactiveDaoTest extends TestCase {
+
+    @Mock
+    private UserReactiveDao userReactiveDao;
+
+
+    @Test
+    public void testFindByEmail() {
+        // Sample email
+        String email = "andy@example.com";
+
+        // Sample User
+        User user = new User();
+        user.setUserId(1);
+        user.setEmail(email);
+
+        // Mock
+        when(userReactiveDao.findByEmail(anyString())).thenReturn(Mono.just(user));
+
+        // Invoke method
+        Mono<User> result = userReactiveDao.findByEmail(email);
+
+        // Verify the result
+        StepVerifier.create(result)
+                .expectNext(user)
+                .verifyComplete();
+
+    }
+
+    @Test
+    public void testFindByUserId() {
+        // Sample user ID
+        int userId = 1;
+
+        // Sample User
+        User user = new User();
+        user.setUserId(userId);
+
+        // Mock
+        when(userReactiveDao.findByUserId(anyInt())).thenReturn(Mono.just(user));
+
+        // Invoke method
+        Mono<User> result = userReactiveDao.findByUserId(userId);
+
+        // Verify the result
+        StepVerifier.create(result)
+                .expectNext(user)
+                .verifyComplete();
+    }
+}
