a9febb94e11698087e431f7b028a26f01c7afbaf
nam.dinh <dragonsang10a1@gmail.com>
2023-05-31 13:33:13 +0700

    #45 API-4910 Refactor code for subscribeToUpdates

--- src/main/java/org/example/service/FriendShipReactiveService.java
+++ src/main/java/org/example/service/FriendShipReactiveService.java
@@ -3,6 +3,7 @@ package org.example.service;
 import org.example.dto.CommonFriendDTO;
 import org.example.dto.FriendConnection;
 import org.example.dto.FriendListDTO;
+import org.example.dto.SubscribeUpdatesDTO;
 import org.example.exception.InvalidEmailException;
 import org.example.model.*;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +13,7 @@ public interface FriendShipReactiveService {
     Mono<ResponseEntity<ResponseObject>> getFriendsListByEmail(FriendListDTO.Request request) throws InvalidEmailException;
     Mono<ResponseEntity<ResponseObject>> getCommonFriends(CommonFriendDTO.Request request) throws InvalidEmailException;
     Mono<ResponseEntity<ResponseObject>> createFriendConnection(FriendConnection.Request request) throws InvalidEmailException;
-    Mono<ResponseEntity<ResponseObject>> subscribeToUpdates(String subscriberEmail, String targetEmail) throws InvalidEmailException;
+    Mono<ResponseEntity<ResponseObject>> subscribeToUpdates(SubscribeUpdatesDTO.Request request) throws InvalidEmailException;
     Mono<Void> blockUpdates(String blockerEmail, String blockedEmail) throws InvalidEmailException;
     Mono<ResponseEntity<ResponseObject>> getEligibleEmailAddresses(String senderEmail) throws InvalidEmailException;
 }
